bc712f535b00e82996a4daf68890043283aa0be6
Hamim-1 <hmd874264@gmail.com>
2026-01-10 07:04:22 +0600

    fix:build issue

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
     <>
       <Hero />
       <Feature />
-      <TopRanking />
+      {/* <TopRanking /> */}
       <NewArraivalProduct />
       <Offer />
       <RecommendProduct />
