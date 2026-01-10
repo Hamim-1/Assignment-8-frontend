8e78e4e26e8246b2b2db42097ac60a50942dc331
Hamim-1 <hmd874264@gmail.com>
2026-01-10 07:08:39 +0600

    fix:build issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  staticPageGenerationTimeout: 180,
   reactCompiler: true,
   images: {
     remotePatterns: [
