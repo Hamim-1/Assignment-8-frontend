9e019417f195d7dcb183b729e5ee1e88c1db28ae
Hamim-1 <hmd874264@gmail.com>
2026-01-19 18:42:02 +0600

    show order history and canlcelled history

--- src/components/modules/Dashboard/User/OrderHistory.tsx
+++ src/components/modules/Dashboard/User/OrderHistory.tsx
@@ -1,12 +1,75 @@
+"use client";
+
 import OrderCard from "@/components/shared/OrderCard";
+import { useAuth } from "@/context/AuthContext";
+import { useEffect, useState } from "react";
+import ProductCardSkeleton from "../../Product/ProductCardSkeleton";
 
 
 const OrderHistory = () => {
+    const { user } = useAuth();
+
+    const [orders, setOrders] = useState<any[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (!user?._id) return;
+
+        const fetchData = async () => {
+            try {
+                setLoading(true);
+                setError(null);
+
+                const res = await fetch(
+                    `${process.env.NEXT_PUBLIC_API_BASE_URL}/orders/history/${user._id}`
+                );
+
+                if (!res.ok) {
+                    throw new Error("Failed to fetch orders");
+                }
+
+                const result = await res.json();
+                setOrders(result.data);
+            } catch (err: any) {
+                setError(err.message || "Something went wrong");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [user?._id]);
+    console.log(orders);
 
     return (
-        <div className="w-full flex flex-col mx-auto space-y-5" id="my-order-section">
-            <p className='text-center text-yellow-500 text-2xl pt-10'>This feature is coming soon!</p>
-            {/* <OrderCard /> */}
+        <div
+            className="w-full flex flex-col mx-auto space-y-5"
+            id="my-order-section"
+        >
+
+            {loading && <ProductCardSkeleton />}
+
+
+            {!loading && error && (
+                <p className="text-center text-red-500 pt-10">
+                    {error}
+                </p>
+            )}
+
+
+            {!loading && !error && orders.length === 0 && (
+                <p className="text-center text-gray-500 pt-10">
+                    You have no orders yet.
+                </p>
+            )}
+
+
+            {!loading && !error && orders.length > 0 && (
+                orders.map((order) => (
+                    <OrderCard key={order._id} order={order} />
+                ))
+            )}
         </div>
     );
 };
--- src/components/shared/OrderCard.tsx
+++ src/components/shared/OrderCard.tsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
-import iphone12 from "@/assets/images/iphone-12.png"
-const OrderCard = () => {
-    const orderProduct =
-    {
-        orderNumber: "ORD12345",
-        date: "2025-12-11",
-        quantity: 2,
-        totalPrice: "50",
-        color: "text-green-500",
-        status: "Delivered",
-    };
+const OrderCard = ({ order }: { order: any }) => {
     return (
         <div className="p-5 border border-gray-300 rounded-sm relative flex flex-col space-y-5">
-            <div>
-                <Image src={iphone12} alt="product-img" className="h-16 w-auto" />
+            <div className="flex space-x-5">
+                {
+                    order.products.map((product: any, i: number) => (
+
+                        <Image
+                            src={product.image}
+                            alt="product-img"
+                            className="h-16 w-auto"
+                            width={64}
+                            height={64}
+                        />
+
+                    ))
+                }
             </div>
 
             <div className="absolute bottom-0 right-0 md:top-5 md:bottom-auto px-5 py-5 md:py-0">
@@ -33,11 +35,21 @@ const OrderCard = () => {
                 </div>
 
                 <div className="flex justify-between">
-                    <p className="w-1/2 sm:h-1/3 md:w-1/5">{orderProduct.orderNumber}</p>
-                    <p className="w-1/2 sm:h-1/3 md:w-1/5">{orderProduct.date}</p>
-                    <p className="hidden sm:block w-1/3 md:w-1/5">x{orderProduct.quantity}</p>
-                    <p className="hidden md:block md:w-1/5">{orderProduct.totalPrice}</p>
-                    <p className="hidden md:block md:w-1/5 {orderProduct.color} font-medium">{orderProduct.status}</p>
+                    <p className="w-1/2 sm:h-1/3 md:w-1/5">
+                        {order._id.slice(0, 7)}...
+                    </p>
+
+                    <p className="w-1/2 sm:h-1/3 md:w-1/5">
+                        {new Date(order.createdAt).toLocaleDateString("en-US", {
+                            year: "numeric",
+                            month: "short",
+                            day: "numeric",
+                        })}
+                    </p>
+
+                    <p className="hidden sm:block w-1/3 md:w-1/5">x{order.products.length}</p>
+                    <p className="hidden md:block md:w-1/5">{order.payment.amount.toFixed(2)}</p>
+                    <p className="hidden md:block md:w-1/5 {orderProduct.color} font-medium capitalize">{order.status}</p>
                 </div>
 
             </div>
@@ -45,10 +57,10 @@ const OrderCard = () => {
 
             <div className="md:hidden flex flex-col space-y-1">
                 <div className="flex space-x-2 sm:space-x-0">
-                    <p className="sm:hidden">x{orderProduct.quantity}</p>
-                    <p className="font-semibold">{orderProduct.totalPrice}</p>
+                    <p className="sm:hidden">x{order.products.length}</p>
+                    <p className="font-semibold">{order.payment.amount.toFixed(2)}</p>
                 </div>
-                <p className="text-primary font-medium">{orderProduct.status}</p>
+                <p className="text-primary font-medium capitalize">{order.status}</p>
             </div>
         </div>
     );
